87ccefd6237a57a0a1a50faa66dafac8f732e77f
thanhjmg <thanhjmg12a3@gmail.com>
2021-11-23 14:03:18 +0700

    Thanh 23/11

--- QuanLyBanThuocClient/src/app/AddObject.java
+++ QuanLyBanThuocClient/src/app/AddObject.java
@@ -32,22 +32,41 @@ public class AddObject {
 			System.setSecurityManager(new SecurityManager());
 		}
 		
-		CTHDDao cthdDao =  (CTHDDao) Naming.lookup("rmi://192.168.1.9:9999/cthdDao");
-		HoaDonDao hoaDonDao =  (HoaDonDao) Naming.lookup("rmi://192.168.1.9:9999/hoaDonDao");
-	    KhachHangDao khachHangDao = (KhachHangDao) Naming.lookup("rmi://192.168.1.9:9999/khachHangDao");
-		LoaiThuocDao loaiThuocDao =  (LoaiThuocDao) Naming.lookup("rmi://192.168.1.9:9999/loaiThuocDao");
-		NhaCungCapDao NCCDao =  (NhaCungCapDao) Naming.lookup("rmi://192.168.1.9:9999/nhaCungCapDao");
-		NhanVienDao nhanVienDao =  (NhanVienDao) Naming.lookup("rmi://192.168.1.9:9999/nhanVienDao");
-		NuocSXDao nuocSXDao =  (NuocSXDao) Naming.lookup("rmi://192.168.1.9:9999/nuocSXDao");
-		TaiKhoanDao tkDao =  (TaiKhoanDao) Naming.lookup("rmi://192.168.1.9:9999/taiKhoanDao");
-		ThuocDao thuocDao =  (ThuocDao) Naming.lookup("rmi://192.168.1.9:9999/thuocDao");
+//		CTHDDao cthdDao =  (CTHDDao) Naming.lookup("rmi://192.168.1.9:9999/cthdDao");
+//		HoaDonDao hoaDonDao =  (HoaDonDao) Naming.lookup("rmi://192.168.1.9:9999/hoaDonDao");
+//	    KhachHangDao khachHangDao = (KhachHangDao) Naming.lookup("rmi://192.168.1.9:9999/khachHangDao");
+//		LoaiThuocDao loaiThuocDao =  (LoaiThuocDao) Naming.lookup("rmi://192.168.1.9:9999/loaiThuocDao");
+//		NhaCungCapDao NCCDao =  (NhaCungCapDao) Naming.lookup("rmi://192.168.1.9:9999/nhaCungCapDao");
+//		NhanVienDao nhanVienDao =  (NhanVienDao) Naming.lookup("rmi://192.168.1.9:9999/nhanVienDao");
+//		NuocSXDao nuocSXDao =  (NuocSXDao) Naming.lookup("rmi://192.168.1.9:9999/nuocSXDao");
+//		TaiKhoanDao tkDao =  (TaiKhoanDao) Naming.lookup("rmi://192.168.1.9:9999/taiKhoanDao");
+//		ThuocDao thuocDao =  (ThuocDao) Naming.lookup("rmi://192.168.1.9:9999/thuocDao");
+		
+		//Thanh
+		CTHDDao cthdDao =  (CTHDDao) Naming.lookup("rmi://192.168.1.8:9999/cthdDao");
+		HoaDonDao hoaDonDao =  (HoaDonDao) Naming.lookup("rmi://192.168.1.8:9999/hoaDonDao");
+	    KhachHangDao khachHangDao = (KhachHangDao) Naming.lookup("rmi://192.168.1.8:9999/khachHangDao");
+		LoaiThuocDao loaiThuocDao =  (LoaiThuocDao) Naming.lookup("rmi://192.168.1.8:9999/loaiThuocDao");
+		NhaCungCapDao NCCDao =  (NhaCungCapDao) Naming.lookup("rmi://192.1192.168.1.868.1.9:9999/nhaCungCapDao");
+		NhanVienDao nhanVienDao =  (NhanVienDao) Naming.lookup("rmi://192.168.1.8:9999/nhanVienDao");
+		NuocSXDao nuocSXDao =  (NuocSXDao) Naming.lookup("rmi://192.168.1.8:9999/nuocSXDao");
+		TaiKhoanDao tkDao =  (TaiKhoanDao) Naming.lookup("rmi://192.168.1.8:9999/taiKhoanDao");
+		ThuocDao thuocDao =  (ThuocDao) Naming.lookup("rmi://192.168.1.8:9999/thuocDao");
 		
 		NhanVien nv1 = new NhanVien("NV1", "Phạm Vũ Hoài An", "Nữ", new Date(2000-1900,4-1,20), "0966105479", "60 Thống Nhất, p10, Q.Gò Vấp","Nhân viên bán hàng", 5000000, "Đang lam viec", new TaiKhoan("NV1", "123"));
 		nhanVienDao.addNhanVien(nv1);
+		NhanVien nv2 = new NhanVien("NV2", "Nguyễn Tuấn Thanh", "Nam", new Date(2001-1900,5-1,20), "0374779028", "56 Đường số 4, p Hiệp Bình Phước, Q.Thủ Đức","Nhân viên bán hàng", 5000000, "Đang lam viec", new TaiKhoan("NV2", "123"));
+		nhanVienDao.addNhanVien(nv2);
+		
+		NhanVien nv3 = new NhanVien("NV3", "Phạm Thị Hoa", "Nữ", new Date(2000-1900,6-1,20), "0966712345", "170 Thống Nhất, p10, Q.Gò Vấp","Nhân viên bán hàng", 5000000, "Nghi viec", new TaiKhoan("NV3", "123"));
+		nhanVienDao.addNhanVien(nv3);
 		
 		KhachHang kh1 = new KhachHang("Đoàn Phạm Bích Hợp", "Nữ", new Date(2001-1900,3-1,01), "0708985897");
 		khachHangDao.addKhachHang(kh1);
-		
+		KhachHang kh2 = new KhachHang("Đoàn Thị Thủy", "Nữ", new Date(2001-1900,4-1,01), "0708985977");
+		khachHangDao.addKhachHang(kh2);
+		KhachHang kh3 = new KhachHang("Nguyễn Công Danh", "Nam", new Date(1999-1900,3-1,01), "0374479123");
+		khachHangDao.addKhachHang(kh3);
 	
 		LoaiThuoc lt1 = new LoaiThuoc("Thuốc kháng dị ứng");
 		NuocSX nsx1 = new NuocSX("Pháp");
